50b5056ea57ebee611a1c2d36da5273842cd1b49
Micheal Parks <michealparks1989@gmail.com>
2024-03-30 12:52:02 -0400

    cleanup

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { Mesh } from 'three'
 	import { T, useTask } from '@threlte/core'
+
 	export let onClick: () => void
 	let ref: Mesh
 	useTask((dt) => {
@@ -9,7 +10,11 @@
 	})
 </script>
 
-<T.PerspectiveCamera makeDefault></T.PerspectiveCamera>
+<T.PerspectiveCamera
+	makeDefault
+	position={[1, 1, 1]}
+	on:create={({ ref }) => ref.lookAt(0, 0, 0)}
+/>
 
 <T.DirectionalLight />
 <T.AmbientLight />
