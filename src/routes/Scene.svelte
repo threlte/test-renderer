c1abc1ef9f6d3a1e49d0ebe50bfc22938e69923b
Micheal Parks <michealparks1989@gmail.com>
2024-03-30 12:38:47 -0400

    sveltekit boilerplate

---
+++ src/routes/Scene.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { Mesh } from 'three'
+	import { T, useTask } from '@threlte/core'
+	export let onClick: () => void
+	let ref: Mesh
+	useTask((dt) => {
+		ref.rotation.x += dt
+		ref.rotation.y += dt
+	})
+</script>
+
+<T.PerspectiveCamera makeDefault></T.PerspectiveCamera>
+
+<T.DirectionalLight />
+<T.AmbientLight />
+
+<T.Mesh bind:ref on:click={onClick}>
+	<T.MeshStandardMaterial />
+	<T.BoxGeometry />
+</T.Mesh>
