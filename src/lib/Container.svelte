73f6c077a49d34b10c3e5491a09ce3867c35ddf5
Micheal Parks <michealparks1989@gmail.com>
2024-04-09 16:39:20 -0400

    remove all unnecessary mocks

--- src/lib/Container.svelte
+++ src/lib/Container.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import { ACESFilmicToneMapping, type WebGLRenderer } from 'three'
-	import type { SvelteComponent } from 'svelte'
+	import { getContext, type SvelteComponent } from 'svelte'
 	import { writable } from 'svelte/store'
-	import { SceneGraphObject } from '@threlte/core'
+	import { SceneGraphObject, createThrelteContext } from '@threlte/core'
 	import { interactivity } from '@threlte/extras'
-	import { createContexts } from './createContexts'
-	import { Scheduler } from './frame-scheduling'
 
-	// @TODO(mp): createThrelteContext should be exposed in `@threlte/core`, for this and also for HUD
-	const contexts = createContexts({
+	export let canvas: HTMLCanvasElement = document.createElement('canvas')
+	export let component: typeof SvelteComponent
+
+	const context = createThrelteContext({
 		colorSpace: 'srgb',
 		toneMapping: ACESFilmicToneMapping,
 		dpr: 1,
@@ -21,47 +21,33 @@
 		useLegacyLights: false
 	})
 
-	contexts.getCtx().renderer = {
-		domElement: document.createElement('canvas')
-	} as WebGLRenderer
-
-	// @TODO: Expose Scheduler from @threlte/core to avoid copying it here.
-	const scheduler = new Scheduler()
-	// @ts-expect-error
-	contexts.getCtx().mainStage = scheduler.createStage(Symbol('threlte-main-stage'))
-	// @ts-expect-error
-	contexts.getCtx().renderStage = scheduler.createStage(Symbol('threlte-render-stage'), {
-		after: contexts.ctx.mainStage,
-		callback(_, runTasks) {
-			if (contexts.ctx.shouldRender()) runTasks()
-		}
-	})
+	context.renderer = { domElement: canvas } as WebGLRenderer
 
 	interface AdvanceOptions {
 		count?: number
 		delta?: number
 	}
 
-	contexts.ctx.advance = (options: AdvanceOptions = {}) => {
-		contexts.getInternalCtx().dispose()
+	const internalContext = getContext('threlte-internal-context')
+
+	context.advance = (options: AdvanceOptions = {}) => {
+		internalContext.dispose()
 
 		for (let i = 0, c = options.count ?? 1; i < c; i += 1) {
 			// @TODO(mp) Expose lastTime (marked private)? Allow more control over deltas in the run() call?
 			// @ts-expect-error
-			scheduler.lastTime = 0
-			scheduler.run(options.delta ?? 16)
+			context.scheduler.lastTime = 0
+			context.scheduler.run(options.delta ?? 16)
 		}
 
-		contexts.getInternalCtx().resetFrameInvalidation()
+		internalContext.resetFrameInvalidation()
 	}
 
 	interactivity({
 		compute: () => undefined
 	})
-
-	export let component: typeof SvelteComponent
 </script>
 
-<SceneGraphObject object={contexts.ctx.scene}>
+<SceneGraphObject object={context.scene}>
 	<svelte:component this={component} {...$$restProps} />
 </SceneGraphObject>
--- src/lib/frame-scheduling/index.ts
+++
@@ -1,4 +0,0 @@
-export { Scheduler, type Schedule, type CreateStageOptions } from './Scheduler'
-export { Stage } from './Stage'
-export { Task, type TaskCallback } from './Task'
-export { type Key, DAG } from './DAG'
