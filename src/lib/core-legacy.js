7e4598c828a018204893288dcfcdc8553140a76f
Micheal Parks <michealparks1989@gmail.com>
2024-06-03 13:51:41 -0400

    swallow multiple instances error

--- src/lib/core-legacy.js
+++ src/lib/core-legacy.js
@@ -4,9 +4,6 @@
  * Supports Svelte <= 4. See `core.svelte.js` for more details.
  */
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as Svelte from 'svelte'
-
 export const LegacyCore = {
 	/**
 	 * Allowed options for the component constructor.
@@ -20,10 +17,10 @@ export const LegacyCore = {
 	 * The `onDestroy` callback is included for strict backwards compatibility
 	 * with previous versions of this library. It's mostly unnecessary logic.
 	 *
-	 * @param {typeof Svelte.SvelteComponent} ComponentConstructor
-	 * @param {Svelte.ComponentConstructorOptions} componentOptions
+	 * @param {typeof import('svelte').SvelteComponent} ComponentConstructor
+	 * @param {import('svelte').ComponentConstructorOptions} componentOptions
 	 * @param {*} onDestroy
-	 * @returns {Svelte.SvelteComponent}
+	 * @returns {import('svelte').SvelteComponent}
 	 */
 	renderComponent: (ComponentConstructor, componentOptions, onDestroy) => {
 		const component = new ComponentConstructor(componentOptions)
@@ -38,7 +35,7 @@ export const LegacyCore = {
 	/**
 	 * Update the component's props.
 	 *
-	 * @param {Svelte.SvelteComponent} component
+	 * @param {import('svelte').SvelteComponent} component
 	 * @param {*} nextProps
 	 */
 	updateProps: (component, nextProps) => {
@@ -48,7 +45,7 @@ export const LegacyCore = {
 	/**
 	 * Remove the component from the DOM.
 	 *
-	 * @param {Svelte.SvelteComponent} component
+	 * @param {import('svelte').SvelteComponent} component
 	 */
 	cleanupComponent: (component) => {
 		component.$destroy()
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,20 @@
+if (typeof window !== 'undefined') {
+	const { warn: originalWarn } = console
+
+	/**
+	 * @TODO Is this an issue on our end / cleanup / or JSDOM?
+	 * @param  {...unknown} args
+	 */
+	const warn = (...args) => {
+		if (args[0] === 'WARNING: Multiple instances of Three.js being imported.') {
+			return
+		}
+
+		originalWarn.call(console, ...args)
+	}
+
+	// eslint-disable-next-line no-console
+	console.warn = warn
+}
+
 export { render, cleanup, act } from './pure'
