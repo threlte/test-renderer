e3a9a3bd12cc6ec2a099df280d0d74fd2788eb4d
Micheal Parks <michealparks1989@gmail.com>
2026-04-06 17:49:08 -0400

    lint

--- src/lib/props.svelte.js
+++ src/lib/props.svelte.js
@@ -1,22 +1,23 @@
+/** @typedef {Record<string, unknown>} Props */
+
 /**
  * Create a shallowly reactive props object.
  *
  * This allows us to update props on `rerender`
  * without turning `props` into a deep set of Proxy objects.
  *
- * @template {Record<string, unknown>} Props
  * @param {Props} initialProps
  * @returns {[Props, (nextProps: Partial<Props>) => void]}
  */
 const createProps = (initialProps = {}) => {
   let currentProps = $state.raw(initialProps)
 
-  const props = new Proxy(initialProps, {
+  const props = new Proxy(/** @type {Props} */ (initialProps), {
     get(_, key) {
-      return currentProps[key]
+      return currentProps[/** @type {string} */ (key)]
     },
     set(_, key, value) {
-      currentProps[key] = value
+      currentProps[/** @type {string} */ (key)] = value
       return true
     },
     has(_, key) {
@@ -27,6 +28,7 @@ const createProps = (initialProps = {}) => {
     },
   })
 
+  /** @param {Partial<Props>} nextProps */
   const update = (nextProps) => {
     currentProps = { ...currentProps, ...nextProps }
   }
